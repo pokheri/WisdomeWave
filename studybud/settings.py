00cd805716b28394bb29fa27fe5ffb005e37ba91
Dinesh Singh <dineshbisht00008@gmail.com>
2024-09-14 17:19:17 +0530

    all

--- studybud/settings.py
+++ studybud/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-34w62dez)i#3r!nhayvhujj@il+5r3c8=1hp#78gyoshk_lp-s'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
